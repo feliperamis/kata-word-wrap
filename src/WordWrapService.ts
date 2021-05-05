8c98c8187419d9c2fa156fa9cdd1f2f661bd9d5d
Adri Valle <avalle@leadtech.com>
2021-05-05 14:23:02 +0200

    [GREEN]: Hard limit

--- src/WordWrapService.ts
+++ src/WordWrapService.ts
@@ -14,8 +14,39 @@ export class WordWrapService {
         return text.length >= lines;
     }
 
-    wrap(text: string, lines: number) {
-
+    wrap(text: string, lines: number): string {
+        const splitted = text.split(" ");
+        let textLine = "";
+        const arrayOfLines = [];
+        for (let i = 0; i < splitted.length; i++) {
+            const word = splitted[i];
+            console.log(word)
+            let forceJump = false;
+            if (!textLine.length) {
+                textLine = word;
+            } else {
+                if ((textLine + " ").length + word.length > lines) {
+                    forceJump = true;
+                    if (textLine.length < lines) textLine = textLine + " ";
+                } else {
+                    textLine = textLine + " " + word;
+                }
+            }
+            if (textLine.length === lines - 1) textLine = textLine + " ";
+            if (
+                textLine.length === lines || 
+                forceJump || 
+                (i + 1) === splitted.length
+            ) {
+                arrayOfLines.push(textLine);
+                textLine = "";
+                if (forceJump) {
+                    i -= 1;
+                }
+                forceJump = false;
+            }            
+        }
+        console.log(arrayOfLines)
+        return arrayOfLines.join("\n");
     }
-
 }
